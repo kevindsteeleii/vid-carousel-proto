058614a2ec9ddbe3b42796b60046f41f6b100917
kevindsteeleii <anubis523@gmail.com>
2021-02-20 21:30:55 -0500

    added parcel init setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.scss";
+
+const rootEl = document.getElementById("root");
+ReactDOM.render(React.createElement(App), rootEl);
